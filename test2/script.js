7578e945568bf14cf772afbc723ee10b7084ddaa
AndrewGiles <giles.andrew33>
2022-03-15 12:58:12 -0600

    trying to fit the data

--- test2/script.js
+++ test2/script.js
@@ -3,6 +3,8 @@ const RIGHT = 'RIGHT';
 const UP = 'UP';
 const DOWN = 'DOWN';
 
+const MODEL_LOCATION = 'localstorage://my-model-1'
+
 const ACTION_NAMES = {
     LEFT,
     RIGHT,
@@ -20,89 +22,87 @@ const ACTIONS = {
 const ACTIONS_LENGTH = Object.values(ACTIONS).length;
 
 const MAX_STEPS = 50;
-
 const MAX_EPS = 1;
-const MIN_EPS = 0.1;
+const MIN_EPS = 0.05;
+const EP_LOSS = 0.001;
+const DISCOUNT_RATE = 0.95;
+const TRAINING_CYCLES = 1000;
+const LEARNING_RATE = 0.002;
 
 let eps = MAX_EPS;
 
-const model = createOrRetrieveModel();
-tfvis.show.modelSummary({ name: 'Model Summary' }, model);
+const IS_TRAINING = true;
 
-let trainingHistory = [];
+const app = new Vue({
+    el: '#app',
+    data: {
+        trainingHistory: [],
+        gameHistory: [],
+        eps,
+        trainingNumber: 0,
+    },
+    mounted() {
+        this.run();
+    },
+    methods: {
+        async run() {
+            const model = await createOrRetrieveModel();
+            tfvis.show.modelSummary({ name: 'Model Summary' }, model);
 
-async function run() {
+            if (IS_TRAINING) {
+                while (this.trainingNumber < TRAINING_CYCLES) {
 
-    let gameNum = 0
+                    const game = await this.trainGame(model);
 
-    while (gameNum < 1000) {
+                    this.trainingHistory = [...this.trainingHistory, game.length].slice(-50);
 
-        const gameLength = await trainGame();
+                    this.trainingNumber++;
 
-        trainingHistory.push(gameLength);
-
-        const values = trainingHistory.map((matchLength, i) => ({
-            x: i,
-            y: matchLength,
-        }));
-
-        tfvis.render.scatterplot(
-            { name: 'Game # v Game Length' },
-            { values },
-            {
-                xLabel: 'Game #',
-                yLabel: 'Length',
-                height: 300
+                    // if (this.trainingNumber % 100 === 0) {
+                    //     await model.save(MODEL_LOCATION);
+                    //     console.log('SAVED MODEL');
+                    // }
+                    if (this.trainingNumber % 100 === 0) this.gameHistory = game;
+                }
+            } else {
+                this.eps = 0;
+                const game = await this.trainGame(model);
+                console.log({ game: game.map(([board]) => board) })
+                this.gameHistory = game;
             }
-        );
-        gameNum++;
+        },
+        async trainGame(model) {
+            tf.engine().startScope();
+            let gameHistory = [];
+
+            let gameBoard = createNewMinimalBoard();
+
+            // We don't want to start with a "winning" board.
+            while (hasWon(gameBoard)) {
+                gameBoard = createNewMinimalBoard();
+            }
+
+            let step = 0;
+            // once we have a valid starting state, we want to train the model
+            while (step < MAX_STEPS && !hasWon(gameBoard)) {
+                const availableActions = getAvailableActions(gameBoard);
+                const actionIndex = await predictAction(model, gameBoard, this.eps, availableActions);
+                const reward = hasWon(gameBoard) ? 50 : -1;
+                const newBoard = takeAction(gameBoard, actionIndex);
+                gameHistory.push([gameBoard, actionIndex, reward, newBoard]);
+                gameBoard = newBoard;
+                step++;
+            }
+            this.eps = Math.max(this.eps - EP_LOSS, MIN_EPS);
+
+            await trainModel(model, [...gameHistory]);
+
+            tf.engine().endScope();
+
+            return gameHistory;
+        }
     }
-
-
-
-    // // More code will be added below
-    // // Convert the data to a form we can use for training.
-    // const tensorData = convertToTensor(data);
-    // const { inputs, labels } = tensorData;
-
-    // // Train the model
-    // await trainModel(model, inputs, labels);
-    console.log('Done Training');
-
-    // // Make some predictions using the model and compare them to the
-    // // original data
-    // testModel(model, data, tensorData);
-}
-
-document.addEventListener('DOMContentLoaded', run);
-
-async function trainGame() {
-    let gameHistory = [];
-
-    let gameBoard = createNewMinimalBoard();
-
-    // We don't want to start with a "winning" board.
-    while (hasWon(gameBoard)) {
-        gameBoard = createNewMinimalBoard();
-    }
-
-    let step = 0;
-    // once we have a valid starting state, we want to train the model
-    while (step < MAX_STEPS && !hasWon(gameBoard)) {
-        const availableActions = getAvailableActions(gameBoard);
-        const actionIndex = await predictAction(model, gameBoard, availableActions);
-        const reward = hasWon(gameBoard) ? 10 : -1;
-        const newBoard = takeAction(gameBoard, actionIndex);
-        gameHistory.push([gameBoard, actionIndex, reward, newBoard]);
-        gameBoard = newBoard;
-        step++;
-    }
-    eps = Math.max(eps - 0.01, MIN_EPS);
-    
-    await trainModel(gameHistory);
-
-    return gameHistory.length;
-}
+})
 
 function createNewMinimalBoard() {
     const selectedPos = {
@@ -137,7 +137,7 @@ function getAvailableActions(board) {
     return availableActions;
 }
 
-async function predictAction(model, gameBoard, availableActions) {
+async function predictAction(model, gameBoard, eps, availableActions) {
     if (Math.random() < eps) {
         const availableActionIndex = Math.floor(Math.random() * availableActions.length);
         return availableActions[availableActionIndex];
@@ -145,7 +145,6 @@ async function predictAction(model, gameBoard, availableActions) {
 
     const predictions = await model.predict(convertBoardToTensor(gameBoard));
     const validPredictions = predictions.dataSync().map((prediction, i) => availableActions.includes(i) ? prediction : -1);
-    console.log({validPredictions})
     return tf.tensor(validPredictions).argMax().dataSync()[0];
 }
 
@@ -187,25 +186,33 @@ function getNewLocation(currentLocation, actionIndex) {
     return newLocation;
 }
 
-function createOrRetrieveModel() {
-    // Create a sequential model
-    const model = tf.sequential();
+async function createOrRetrieveModel() {
 
-    // Add a single input layer
-    model.add(tf.layers.dense({ name: 'input-layer', inputShape: [4, 8], units: 25 }));
-    model.add(tf.layers.flatten());
-    model.add(tf.layers.dense({ name: 'hidden-1', units: 75, activation: 'sigmoid' }));
-    model.add(tf.layers.dense({ name: 'hidden-2', units: 50, activation: 'sigmoid' }));
-    model.add(tf.layers.dense({ name: 'hidden-3', units: 25, activation: 'sigmoid' }));
+    try {
+        const model = await tf.loadLayersModel(MODEL_LOCATION);
+        console.log('Found existing model and loaded it');
+        return model
+    }
+    catch (error) {
+        console.error('Could Not find model. Creating New One');
+        const model = tf.sequential();
 
-    // Add an output layer
-    model.add(tf.layers.dense({ name: 'output-layer', units: 4, activation: 'softmax' }));
+        // Add a single input layer
+        model.add(tf.layers.dense({ name: 'input-layer', inputShape: [4, 8], units: 50 }));
+        model.add(tf.layers.flatten());
+        model.add(tf.layers.dense({ name: 'hidden-1', units: 100, activation: 'tanh' }));
+        model.add(tf.layers.dense({ name: 'hidden-2', units: 100, activation: 'tanh' }));
+        model.add(tf.layers.dense({ name: 'hidden-3', units: 100, activation: 'tanh' }));
 
-    return model;
+        // Add an output layer
+        model.add(tf.layers.dense({ name: 'output-layer', units: 4, activation: 'softmax' }));
+
+        return model;
+    }
 }
 
 function convertBoardToTensor(board) {
-    return tf.tensor3d([board], [1,4, 8]);
+    return tf.tensor3d([board], [1, 4, 8]);
 }
 
 function convertToTensor(gameHistory) {
@@ -217,9 +224,17 @@ function convertToTensor(gameHistory) {
     const labels = gameHistory.map(([, actionIndex,]) => tf.oneHot(actionIndex, ACTIONS_LENGTH).dataSync());
     const rewards = gameHistory.map(([, , rewards]) => rewards);
 
+    let discountedRewards = []
+    let discountedSum = 0
+
+    for (reward in rewards.reverse()) {
+        discountedSum = reward + DISCOUNT_RATE * discountedSum
+        discountedRewards.push(discountedSum)
+    }
+
     const inputTensor = tf.tensor3d(inputs, [inputs.length, inputs[0].length, inputs[0][0].length]);
     const labelTensor = tf.tensor2d(labels, [labels.length, labels[0].length]);
-    const rewardsTensor = tf.tensor2d(rewards, [rewards.length, 1]);
+    const rewardsTensor = tf.tensor2d(discountedRewards.reverse(), [rewards.length, 1]);
 
     return {
         inputs: inputTensor,
@@ -228,15 +243,15 @@ function convertToTensor(gameHistory) {
     }
 }
 
-async function trainModel(gameHistory) {
+async function trainModel(model, gameHistory) {
     // Prepare the model for training.
     model.compile({
-        optimizer: 'adam',
+        optimizer: tf.train.sgd(LEARNING_RATE),
         loss: 'meanSquaredError',
     });
 
-    const batchSize = 32;
-    const epochs = 5;
+    const batchSize = 1;
+    const epochs = 1;
 
     const { inputs, labels, rewards } = convertToTensor(gameHistory);
 
@@ -245,55 +260,5 @@ async function trainModel(gameHistory) {
         epochs,
         shuffle: true,
         weights: rewards,
-        callbacks: tfvis.show.fitCallbacks(
-            { name: 'Training Performance' },
-            ['loss', 'mse'],
-            { height: 200, callbacks: ['onEpochEnd'] }
-        )
     });
 }
-
-// function testModel(model, inputData, normalizationData) {
-//     const { inputMax, inputMin, labelMin, labelMax } = normalizationData;
-
-//     // Generate predictions for a uniform range of numbers between 0 and 1;
-//     // We un-normalize the data by doing the inverse of the min-max scaling
-//     // that we did earlier.
-//     const [xs, preds] = tf.tidy(() => {
-
-//         const xs = tf.linspace(0, 1, 100);
-//         const preds = model.predict(xs.reshape([100, 1]));
-
-//         const unNormXs = xs
-//             .mul(inputMax.sub(inputMin))
-//             .add(inputMin);
-
-//         const unNormPreds = preds
-//             .mul(labelMax.sub(labelMin))
-//             .add(labelMin);
-
-//         // Un-normalize the data
-//         return [unNormXs.dataSync(), unNormPreds.dataSync()];
-//     });
-
-
-//     const predictedPoints = Array.from(xs).map((val, i) => {
-//         return { x: val, y: preds[i] }
-//     });
-
-//     const originalPoints = inputData.map(d => ({
-//         x: d.horsepower, y: d.mpg,
-//     }));
-
-
-//     tfvis.render.scatterplot(
-//         { name: 'Model Predictions vs Original Data' },
-//         { values: [originalPoints, predictedPoints], series: ['original', 'predicted'] },
-//         {
-//             xLabel: 'Horsepower',
-//             yLabel: 'MPG',
-//             height: 300
-//         }
-//     );
-// }
-
